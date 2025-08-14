0cfa672b6faf85a84c26e264075b60efe2eb66f2
M1gu3lDev <miguel.moura@dcx.ufpb.br>
2025-08-13 21:09:57 -0300

    adicionando campos picture e category

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.phone})"
